fdbd8a4d39b08c49f3415cc7e3a0ae4537e099e5
gabrielc42 <clark.gabe97@gmail.com>
2022-10-26 19:10:05 -0500

    nightly install, initial mars weight calc project

---
+++ mars-calc/target/debug/deps/mars_calc.d
@@ -0,0 +1,5 @@
+C:\Users\Gabriel\Desktop\reposit\rust\mars-calc\target\debug\deps\mars_calc.exe: src\main.rs
+
+C:\Users\Gabriel\Desktop\reposit\rust\mars-calc\target\debug\deps\mars_calc.d: src\main.rs
+
+src\main.rs:
---
+++ mars-calc/target/debug/mars-calc.d
@@ -0,0 +1,1 @@
+C:\Users\Gabriel\Desktop\reposit\rust\mars-calc\target\debug\mars-calc.exe: C:\Users\Gabriel\Desktop\reposit\rust\mars-calc\src\main.rs
