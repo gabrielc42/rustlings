11df7eece78c66cb30e3f9c54455439a3686613c
gabrielc42 <clark.gabe97@gmail.com>
2022-10-26 20:17:24 -0500

    mars calc program, borrowing/ownership/mutability lessons

--- mars-calc/src/main.rs
+++ mars-calc/src/main.rs
@@ -1,10 +1,21 @@
 use std::io;
 
 fn main() {
+    // let s1 = &input;
+    // let s2 = &input;
+    // println!("{} {}", s1, s2);
+
+    // some_fn(&mut input);
+
+    println!("Enter your weight in kilograms: ");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
+    
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    let mars_weight = calculate_weight_on_mars(weight);
+    // mars_weight = mars_weight * 1000.0; in grams
     println!("Weight on Mars: {}g", mars_weight);
 }
 
@@ -15,3 +26,9 @@ fn calculate_weight_on_mars(weight:f32) -> f32 {
 // 1. Each value in Rust is owned by a variable.
 // 2. When the owner goes out of scope, the value will be deallocated
 // 3. There can only be one owner at a time
+
+// a single mutable borrow or as many immutable borrows
+
+// fn some_fn(s: &mut String) {
+//     s.push_str("a");
+// }
