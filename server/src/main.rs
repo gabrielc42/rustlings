bce5ed94d7bd7117894467a6c9b9f88d804c79d0
gabrielc42 <clark.gabe97@gmail.com>
2022-10-27 21:52:29 -0500

    error for string reference

---
+++ server/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr //if name of struct variable is same as fn param, can just pass this. other wise addr: addr
+        }
+    }
+
+    fn run(self) { //self is like 'this' of java
+        
+    }
+}
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+C:\Users\Gabriel\Desktop\reposit\rust\server\target\debug\server.exe: C:\Users\Gabriel\Desktop\reposit\rust\server\src\main.rs
